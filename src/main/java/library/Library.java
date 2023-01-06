4c46a76841a7d8ab684590a28502099af1105ce3
Lewis Lovett <llovett7575@gmail.com>
2023-01-06 16:28:38 +0000

    Library Class converts CSV to JSON file

--- src/main/java/library/Library.java
+++ src/main/java/library/Library.java
@@ -1,34 +1,35 @@
 package library;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import org.json.CDL;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 public class Library {
 
     private ArrayList<Book> Books = new ArrayList<>();
 
     public Library() {
-        populateLibrary();
+        createJSONBooks();
     }
 
-    public void populateLibrary(){
-        String line = "";
-        String splitBy ="";
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("src/main/java/org/example/books_data.csv"));
-            br.readLine();
-            while((line = br.readLine())!= null) {
-                String[] bookInfo = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-                Book newBook = new Book(Integer.parseInt(bookInfo[0]),bookInfo[1],bookInfo[2],bookInfo[3], bookInfo[4], bookInfo[5] );
-                Books.add(newBook);
-            }
-        }
+    public void createJSONBooks(){
 
-        catch (IOException e) {
+        InputStream inputStream = Library.class.getClassLoader().getResourceAsStream("books_data.csv");
+        String csvAsString = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+        String json = CDL.toJSONArray(csvAsString).toString();
+        try {
+            Files.write(Path.of("src/main/resources/books_data.json"), json.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -41,6 +42,6 @@ public class Library {
 
     public static void main(String[] args) {
         Library newLibrary = new Library();
-        newLibrary.printBooksInfomation();
+//        newLibrary.printBooksInfomation();
     }
 }
